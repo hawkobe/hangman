e6b9a487d87de8b74b7eed7524efa7ef0508f871
Jacob Sanders <sanders.jms@gmail.com>
2023-02-01 14:57:54 -0700

    setting up messages.rb and figuring out how to select word from dicitonary

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,14 @@
+require 'colored2'
+require_relative 'messages.rb'
+
 class Hangman
+  include Messages
+  attr_reader :dictionary
   def initialize
-    @dictionary = File.readlines('google-10000-english-no-swears.txt')
+    @dictionary = File.readlines('google-10000-english-no-swears.txt').map do |line|
+      line.chomp
+    end
   end
 end
 
+game = Hangman.new
